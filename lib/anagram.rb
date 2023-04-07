fa200830e9defc0671cae0866dc9c479747be6a7
Daniel Guzman <guzdan@live.com>
2023-04-06 20:10:17 -0500

    commit tree f87ab1dffc4664fc96c173cd05b5d2dce4e087f9

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Anagram
+
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end 
+
+    def match(words_array)
+       return words_array.filter do |w|
+            w.chars.sort == word.chars.sort
+        end
+    end
+end
+
+listen = Anagram.new("listen")
+pp listen.match(%w[enlists google inlets banana])
